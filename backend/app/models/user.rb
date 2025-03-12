20502de2a63cd0f2448ea633c34e2b9e37be8cdd
imrie123 <nosirie@ymobile.ne.jp>
2025-03-13 04:12:15 +0900

    Add migrations to create users table and add password column

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,4 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  has_secure_password
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
 end
